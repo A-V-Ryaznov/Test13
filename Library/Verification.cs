38c952b2054f06e34fc30a85f43d0c61a38da19e
Оганесьянц Артем <a.oganesyants2003@gmail.com>
2023-04-21 12:04:44 +0300

    Добавьте файлы проекта.

---
+++ Library/Verification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnApp.Library
+{
+    public static class Verification
+    {
+        public static bool ValidateUserData(string login, string password)
+        {
+           foreach(var item in App.DataBase.User)
+            {
+                if (login.Equals(item.Login) && password.Equals(item.Password))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
